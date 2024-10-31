8e3f3493cf99265e7054fc91687a07e4e1865142
Vexonor <vexonordark@gmail.com>
2024-10-31 16:42:27 +0700

    Make Responsive

--- app/components/floating-frameW.tsx
+++ app/components/floating-frameW.tsx
@@ -1,14 +1,5 @@
 import React from "react";
 import { FloatingDock } from "./ui/floating-dock";
-import {
-    IconBrandGithub,
-    IconBrandX,
-    IconExchange,
-    IconHome,
-    IconNewSection,
-    IconTerminal2,
-} from "@tabler/icons-react";
-import Image from "next/image";
 
 export function FloatingDockFrame() {
     const links = [
